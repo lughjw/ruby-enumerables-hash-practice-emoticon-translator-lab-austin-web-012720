eb4ddd3c5248be54667df3c37ccb94f376b61057
Lugh Wilson <lughjw@gmail.com>
2020-01-03 23:59:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  temp = YAML.load_file(file_path)
+  emoticons = {
+    :get_meaning =>  {},
+    :get_emoticon => {}
+  }
+  
+  temp.each_pair do |meaning,emoticon|
+    japanese_emoticon = emoticon[0]
+    english_emoticon  = emoticon[1]
+    emoticons[:get_meaning][english_emoticon]   = meaning.to_s
+    emoticons[:get_emoticon][japanese_emoticon] = english_emoticon
+  end
+  
+  emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  emoticons = load_library(file_path)
+  emoticon[:get_meaning][emoticon]
 end
 
-def get_english_meaning
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  emoticons = load_library(file_path)
+  result = emoticons[:get_emoticon][emoticon]
+  if result == nil
+    result = 
 end
