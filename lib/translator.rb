8d32603dfe7d9057cc9abb0944bfc8e48289ffd0
Lugh Wilson <lughjw@gmail.com>
2020-01-04 00:03:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ end
 def get_english_meaning(file_path, emoticon)
   puts emoticon
   emoticons = load_library(file_path)
-  emoticon[:get_meaning][emoticon]
+  emoticons[:get_meaning][emoticon]
 end
 
 def get_japanese_emoticon(file_path, emoticon)
